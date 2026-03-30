9dec3dcbf0f9eb25a2985d86da9cc4dc9c864186
adrian5517 <juelboncodin@gmail.com>
2026-03-30 13:15:52 +0800

    All API PHP files now send bulletproof CORS

--- src/api/menu_list.php
+++ src/api/menu_list.php
@@ -1,8 +1,9 @@
 <?php
-header('Content-Type: application/json');
+// Bulletproof CORS headers for all request types
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
+header('Content-Type: application/json');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
